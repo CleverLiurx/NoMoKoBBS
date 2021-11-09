71361babf3ed922d605accbcb39efb44ed6d0609
Liurx <liurongxin_i@didiglobal.com>
2021-11-09 16:05:01 +0800

    :tada: 首次提交

---
+++ app.js
@@ -0,0 +1,16 @@
+import Koa from 'koa'
+import compress from 'koa-compress'
+import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors'
+import servStatic from 'koa-static'
+import config from './config'
+
+const app = new Koa()
+
+app
+  .use(cors())
+  .use(compress({ threshold: 2048 }))
+  .use(servStatic('./build'))
+  .use(bodyParser())
+
+app.listen(config.port, () => console.log(`server started ${config.port}`))
