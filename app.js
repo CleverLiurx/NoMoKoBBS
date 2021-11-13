0b8e7e0406dd04f9a592ed11d6291496d0784fc2
Liurx <liurongxin_i@didiglobal.com>
2021-11-13 13:55:10 +0800

    :sparkles: 增加session

--- app.js
+++ app.js
@@ -7,11 +7,24 @@ import config from './config'
 import router from './router'
 import { mongoConnect } from './db'
 import intercept from './middlewares/intercept'
+import session from 'koa-session'
+
+// session配置
+const session_signed_key = ['some secret hurr']
+const session_config = {
+  key: 'nomokobbs:sess',
+  maxAge: 1000 * 60 * 60 * 24 * 3, // 3 days
+  rolling: true, // 请求时自动刷新
+  renew: false
+}
 
 const app = new Koa()
 
+app.keys = session_signed_key
+
 app
   .use(intercept())
+  .use(session(session_config, app))
   .use(cors())
   .use(compress({ threshold: 2048 }))
   .use(servStatic('./build'))
--- middlewares/intercept/index.js
+++ middlewares/intercept/index.js
@@ -1,7 +1,8 @@
 const isUnAuth = ctx => {
   const url = ctx.request.url
   let flag = false
-  if (url.indexOf('/api/v1/un_auth') > -1) {
+  // un_auth路由和非接口请求(静态资源)不验证session
+  if (url.indexOf('/api/v1/un_auth') > -1 || url.indexOf('/api/v1/') === -1) {
     flag = true
   }
   return flag
