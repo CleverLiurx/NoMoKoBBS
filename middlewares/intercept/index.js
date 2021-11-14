42f4339bf876c4e06882735e069eec591e54ceca
Liurx <liurongxin_i@didiglobal.com>
2021-11-14 11:44:13 +0800

    :lock: 只允许单用户登录

--- middlewares/intercept/index.js
+++ middlewares/intercept/index.js
@@ -1,25 +1,56 @@
-import { redis } from '../../db'
+import { redis, loginTimeKey } from '../../db'
 import { sessionKey, sessionTime } from '../../config/session'
 
+// 路由为/api/v1/un_auth的接口
 const isUnAuth = ctx => {
   const url = ctx.request.url
   let flag = false
-  // un_auth路由和非接口请求(静态资源)不验证session
-  if (url.indexOf('/api/v1/un_auth') > -1 || url.indexOf('/api/v1/') === -1) {
+  if (url.indexOf('/api/v1/un_auth') > -1) {
     flag = true
   }
   return flag
 }
 
+// 接口请求(/api/v1, 非静态资源)
+const isApi = ctx => {
+  const url = ctx.request.url
+  return url.indexOf('/api/v1/') > -1
+}
+
 export default () => {
   return async (ctx, next) => {
-    const sessionId = ctx.cookies.get(sessionKey)
-    // console.log(sessionId)
-    // redis.expire(ctx.cookies.get('nomokobbs:sess'), sessionTime)
-    if (isUnAuth(ctx)) {
+    if (isUnAuth(ctx) || !isApi(ctx)) { // 放行不用验证的路由和静态资源
       await next()
     } else {
-      // TODO: 验证session
+      // 获取session中的userId和loginTime
+      const sessionId = ctx.cookies.get(sessionKey) || 'no_session_id'
+      const { userId, loginTime } = await redis.hgetall(sessionId)
+
+      if (!userId || !loginTime) {
+        const res = {
+          errno: '1006',
+          errmsg: '未登录'
+        }
+        ctx.body = JSON.stringify(res)
+        return false
+      }
+
+      // session与redis中用户的loginTime对比，不一致则发生了多用户登录
+      // TODO: 以后改为登录时消息推送，不在接口中拦截，提升效率
+      const key = loginTimeKey(userId)
+      const time = await redis.get(key)
+      if (time != loginTime) {
+        const res = {
+          errno: '1007',
+          errmsg: '该账号已在另一客户端登录'
+        }
+        ctx.body = JSON.stringify(res)
+        return false
+      }
+      
+      // 刷新session时间
+      redis.expire(sessionId, sessionTime)
+
       await next()
     }
   }
