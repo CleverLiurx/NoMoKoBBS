470fb95c8edf5e457a37553da8af4fcba84db13e
Liurx <liurongxin_i@didiglobal.com>
2022-01-20 10:18:39 +0800

    servStatic

--- src/app.js
+++ src/app.js
@@ -26,12 +26,7 @@ app
   .use(session(sessionConfig, app))
   .use(cors({ credentials: true }))
   .use(compress({ threshold: 2048 }))
-  .use(
-    servStatic(path.join(__dirname, "../build"), {
-      maxage: 1000 * 60 * 60 * 24 * 7,
-    })
-  )
-  // .use(servStatic(path.join(__dirname, "../public")))
+  .use(servStatic(path.join(__dirname, "../build")))
   .use(
     koaBody({
       multipart: true, // 支持文件上传
